363bfd18e8b79b48e066af2340344c664c930a9d
fanwei <fan.wei.xiwen@gmail.com>
2023-11-07 18:24:53 +0000

    adapt to csv
    Signed-off-by: fanwei <fan.wei.xiwen@gmail.com>

--- create_h264_jpeg.py
+++ create_h264_jpeg.py
@@ -1,17 +1,17 @@
-import json
 from os import walk
-import mp4parser
 import os
-import sys
-import io
 from mp4parser import MP4
 import os
 import uuid
 import re
 import subprocess
+import csv
+import numpy as np
 
-dirpath = '/content/drive/MyDrive/internship/byteNet/'
-h264dir = 'vh264/'
+
+
+dirpath = '/home/ubuntu/data/h264_v20231107/'
+h264dir = 'h264/'
 imagedir = 'image/'
 
 h264path = os.path.join(dirpath, h264dir)
@@ -25,6 +25,7 @@ if not isExist:
 
 dirpath = os.path.expanduser(dirpath)
 json_list = []
+id_list = []
 for root, directories, filenames in os.walk(dirpath):
     for filename in filenames:
 
@@ -42,7 +43,8 @@ for root, directories, filenames in os.walk(dirpath):
             except subprocess.CalledProcessError as err:
                 print(err)
 
-            #uid = 'b37fbfd2-83f3-4281-ab36-6be70bc118c0'
+
+            #uid = '428a9041-ffa6-42d4-b2e0-b5df7f9c7978'
             iframe_index = []
             image_files = next(walk(imagepath), (None, None, []))[2]
             for frame_file in image_files:
@@ -63,6 +65,24 @@ for root, directories, filenames in os.walk(dirpath):
 
             for idx, iframe_file in enumerate(iframe_files):
                 json_list.append({'h264': str(iframe_file), 'image': imagepath + image_files[idx], 'video': path})
+            id_list +=  [uid + '-' + str(j) for j in iframe_index]
 
-with open("test.json", "w") as out_file:
-    json.dump(json_list, out_file, indent=6)
+#with open("test.json", "w") as out_file:
+#    json.dump(json_list, out_file, indent=6)
+
+
+list_len = len(id_list)
+np.random.shuffle(id_list)
+trainlist, validlist, testlist = np.split(id_list, 
+                       [int(.8*list_len), int(.9*list_len)])
+with open(dirpath + "dataset.csv",  mode='w') as f:
+
+        writer = csv.writer(f)
+        writer.writerow(['id','flag'])
+        for row in trainlist:
+            writer.writerow([row,'TRAIN'])
+        for row in validlist:
+            writer.writerow([row,'VALIDATE'])
+        for row in testlist:
+            writer.writerow([row,'TEST'])
+
